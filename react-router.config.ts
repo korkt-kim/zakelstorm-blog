ddb3f53d754f536ecb3dfbfe996166c8c1597eff
korkt-kim <gnscott6@naver.com>
2025-12-24 20:25:45 +0900

    feat(core): add SSG scripts and markdown infrastructure

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,7 +1,22 @@
-import type { Config } from "@react-router/dev/config";
+import type { Config } from '@react-router/dev/config'
+import path from 'path'
+import { getSlugsFromMarkdown, removeExtension } from 'utils/fs'
 
 export default {
   // Config options...
   // Server-side render by default, to enable SPA mode set this to `false`
   ssr: true,
-} satisfies Config;
+  prerender: async ({ getStaticPaths }) => {
+    const paths = await getStaticPaths()
+
+    const slugs = await getSlugsFromMarkdown(
+      path.join(process.cwd(), 'contents/')
+    )
+
+    return [
+      '/',
+      ...paths,
+      ...slugs.map(slug => `/articles/${slug[0]}/${removeExtension(slug[1])}`),
+    ]
+  },
+} satisfies Config
